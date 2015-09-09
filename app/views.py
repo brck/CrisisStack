694da341cd73dc8c26680c88be1ecf91a989d45f
Frankie Onuonga <frankie.onuonga@gmail.com>
2015-09-09 10:03:03 +0300

    Applciation routes. This is bascially the controller: - Plug between view and model

---
+++ app/views.py
@@ -0,0 +1,71 @@
+import os, subprocess , shlex , sys
+from app import app , db , models , forms , APP_STATIC 
+from flask import Flask, request, render_template, redirect , url_for , flash 
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', applicationset = models.ApplicationTable.query.filter_by(installed=True))
+
+@app.route('/MyApplications')
+def MyApplications():
+    return render_template("presentapplications.html", presentapplications = models.ApplicationTable.query.filter_by(installed = True))
+
+@app.route('/MyApplications/<int:app_id>')
+def myapplication(app_id):
+	return render_template("applicationprofile.html", appentry = models.ApplicationTable.query.get(app_id))
+
+@app.route('/StoreApplications')
+def Storeapplications():
+    return render_template("StoreApplications.html", storeapplications = models.ApplicationTable.query.filter_by(installed= False), )	  
+
+@app.route('/StoreApplications/<int:app_id>', methods = ['PUT','DELETE'])
+def storeapplication(app_id):
+    return render_template("applicationprofileinstall.html", storeappentry = models.ApplicationTable.query.get(app_id))      
+
+def launchaction():
+    appentry = models.ApplicationTable.query.get(app_id)
+    if request.method == 'GET':
+        if request.form['submit'] == "Install Application" :
+            subprocess.call([{{storeappentry.installscript}}])
+            if ( subprocess.call(["echo $?"]) == 0):
+               appentry.update().\
+               values(installed = 'True') .\
+               where(appentry.id == app_id)
+               flash('The installation process will run in the background. Kindly launch from Installed applications ')
+            else: 
+                pass 
+        elif request.form ['submit'] == "Delete Application" :
+             subprocess.call([{{appentry.uninstallscript}}])
+             if ( subprocess.call(["echo $?"]) == 0):
+               appentry.update().\
+               values(installed = 'False') .\
+               where(appentry.id == app_id)
+               flash ('Your application will be deleted in the background.')
+             else: 
+                pass 
+        else:
+             pass   
+    else: 
+        pass 
+
+@app.route ('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=='GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    if username != "admin" and password != "password" :
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    return redirect(request.args.get('next') or url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    error = 'This page does not exist'
+    return render_template( '404.html', error = error ) 
+	
+
+@app.errorhandler(500)
+def internal_server_error(e):
+	return 'Sorry we are currently experiencing an internal server error', 500
