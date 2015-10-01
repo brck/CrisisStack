3e1c2482459843100b5156b6c27948e699b88c79
Frankie Onuonga <frankie.onuonga@gmail.com>
2015-10-01 10:17:07 +0300

    Change in installs cripts in etherpad as it is not recommended to run it as root

--- app/static/scripts/install/Etherpad.sh
+++ app/static/scripts/install/Etherpad.sh
@@ -1,8 +1,9 @@
 #!/bin/sh 
 sudo apt-get update 
-sudo dkpg -i libraries/node-arm.deb
+sudo dpkg -i libraries/node-arm.deb
 sudo apt-get -y install gzip git-core curl libssl-dev pkg-config build-essential npm
-cd ~
+su pi
+cd /home/pi
 mkdir applications && cd applications 
 git clone https://github.com/ether/etherpad-lite
 cd etherpad-lite
--- app/static/scripts/install/Ushahidi.sh
+++ app/static/scripts/install/Ushahidi.sh
@@ -1,6 +1,40 @@
-#!/bin/sh 
-sudo apt-get -y install libpcre3 libpcre3-dev php5 php5-mcrypt php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-json php5-curl
-sudo apt-get install -y mysql-server mysql-client 
+#!/bin/sh
+
+sudo apt-get  install -y libpcre3 libpcre3-dev php5 php5-mcrypt php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-mhash php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl php5-json php5-curl
+sudo apt-get install -y mysql-server mysql-client
+
+
+echo -n "Enter the MySQL root password: "
+read -s rootpw
+echo -n "Enter database name: "
+read dbname
+echo -n "Enter database username: "
+read dbuser
+echo -n "Enter database user password: "
+read dbpw
+ 
+db="create database $dbname;GRANT ALL PRIVILEGES ON $dbname.* TO $dbuser@localhost IDENTIFIED BY '$dbpw';FLUSH PRIVILEGES;"
+mysql -u root -p$rootpw -e "$db"
+ 
+if [ $? != "0" ]; then
+ echo "[Error]: Database creation failed"
+ exit 1
+else
+ echo "------------------------------------------"
+ echo " Database has been created successfully "
+ echo "------------------------------------------"
+ echo " DB Info: "
+ echo ""
+ echo " DB Name: $dbname"
+ echo " DB User: $dbuser"
+ echo " DB Pass: $dbpw"
+ echo ""
+ echo "------------------------------------------"
+fi
+
+
+
+ 
 cd ~
 git clone https://github.com/ushahidi/Ushahidi_Web.git
 mv Ushahidi_Web /var/www/
