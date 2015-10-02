b66cfc60f582ff986fb13080cd2d247db7b9ed85
Frankie Onuonga <frankie.onuonga@gmail.com>
2015-10-02 19:26:43 +0300

    Ensure that logging to a file is much easier

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 __author__ = 'Frankie'
 import os 
 import logging 
-
-
 from flask import Flask, render_template, request, flash, g, session, redirect, url_for
 from forms import LoginForm
 from decorators import requires_login
@@ -10,8 +8,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from logging import FileHandler
 
 app = Flask(__name__)
+
 applogger = app.logger 
-file_handler = FileHandler("cserror.log")
+file_handler = FileHandler("crisisstackserror.log")
 file_handler.setLevel(logging.DEBUG)
 applogger.setLevel(logging.DEBUG)
 applogger.addHandler(file_handler)
