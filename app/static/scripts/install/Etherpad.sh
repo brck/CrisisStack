1603da28ed6b7c8504281a6e857740a53ee6b98a
Frankie Onuonga <frankie.onuonga@gmail.com>
2015-10-08 18:14:03 +0300

    Taking some weight off the installation script to allow front end to collect its results much faster

--- app/static/scripts/install/Etherpad.sh
+++ app/static/scripts/install/Etherpad.sh
@@ -1,12 +1,10 @@
 #!/bin/sh 
-sudo apt-get update 
+#Leaning the updating to the user. If you have not updated, kindly run this command $ apt-get update  
 ##on ubuntu install nodejs 
 ## on raspbian , install the node library in librariers folder. 
 sudo apt-get -y install gzip git-core curl libssl-dev pkg-config build-essential npm
 su pi
-cd /home/pi
-mkdir applications
-cd applications 
+cd /home/pi/applications 
 git clone https://github.com/ether/etherpad-lite
 cd etherpad-lite
 npm install dirty && npm update
