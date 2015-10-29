644fb738859daaa0b34f688c840deb58afbe3d75
Frankie Onuonga <frankie.onuonga@gmail.com>
2015-10-29 18:51:55 +0300

    Let supervisord handle launch of this to sumilate a start after installation.Therefore move the conf file to the correct place

--- app/static/scripts/install/Etherpad.sh
+++ app/static/scripts/install/Etherpad.sh
@@ -1,11 +1,5 @@
 #!/bin/sh 
-#Leaning the updating to the user. If you have not updated, kindly run this command $ apt-get update  
+#If you have not updated, kindly run this command $ apt-get update  
 ##on ubuntu install nodejs 
 ## on raspbian , install the node library in librariers folder. 
-sudo apt-get -y install gzip git-core curl libssl-dev pkg-config build-essential npm
-su pi
-cd /home/pi/applications 
-git clone https://github.com/ether/etherpad-lite
-cd etherpad-lite
-npm install dirty && npm update
-./bin/run.sh
+cp /home/pi/CrisisStack/app/libraries/Etherpad-lite.conf /etc/supervisor/conf.d/Etherpad-lite.conf
