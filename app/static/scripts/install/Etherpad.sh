093cae58a69d4326141bec67cd21b83e11a0214f
Frankie Onuonga <frankie.onuonga@gmail.com>
2015-10-29 18:54:20 +0300

    Ensure we restart supervisor daemon after the new changes-> placing the config

--- app/static/scripts/install/Etherpad.sh
+++ app/static/scripts/install/Etherpad.sh
@@ -3,3 +3,4 @@
 ##on ubuntu install nodejs 
 ## on raspbian , install the node library in librariers folder. 
 cp /home/pi/CrisisStack/app/libraries/Etherpad-lite.conf /etc/supervisor/conf.d/Etherpad-lite.conf
+service supervisor restart
