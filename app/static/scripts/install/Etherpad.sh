0518108873a8d2b5cca5a627065942536586e5cc
Onuonga <frankie.onuonga@gmail.com>
2016-01-20 18:59:37 +0300

    Branch does not need any python flask dev work and therefore cleaning up

--- app/static/scripts/install/Etherpad.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-#If you have not updated, kindly run this command $ apt-get update  
-##on ubuntu install nodejs 
-## on raspbian , install the node library in librariers folder. 
-cp /home/pi/CrisisStack/app/libraries/Etherpad-lite.conf /etc/supervisor/conf.d/Etherpad-lite.conf
-service supervisor restart
