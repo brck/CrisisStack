6180af3db214231d18ec8a918633967b0a5b9c5a
Onuonga <frankie.onuonga@gmail.com>
2016-01-22 15:20:52 +0300

    Cleaning up my base application routes

--- app.lua
+++ app.lua
@@ -23,9 +23,13 @@ app:match("/MyApplications/:application_id/analytics", function(self)
 	return "This is an applications analytics page "
 end)
 
-app:match("/Search", function(self)
- return "This is the search page"
-end)
+app:match("/Search", respond_to ({
+    GET = function(self)
+         return "This is the search page"
+    end, 
+    POST = function(self),
+    --- return      
+    end}))
 
 app:delete("/MyApplications/:application_id/delete", function(self)
 	return "delete the application"
