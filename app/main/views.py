a69dee09d925e9294fee08d2b13d6f53b2bbe618
eduhg <edwinnyangena@gmail.com>
2016-10-26 14:39:22 +0300

    updated app links in index.html

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,8 @@ from . import main
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/app_info')
+def app_info():
+    return render_template('app_info.html')
