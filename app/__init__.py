15928b6f649d79c073fd79038f43132ecceaa497
eduhg <edwinnyangena@gmail.com>
2016-10-26 13:53:22 +0300

    created and registered  main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,12 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    # Register Auth blueprint to application factory
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     # Register Auth blueprint to application factory
     from .auth import auth as auth_blueprint
