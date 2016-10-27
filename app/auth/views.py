8fe1716bada91eb7d193873e5166250da96f555a
eduhg <edwinnyangena@gmail.com>
2016-10-27 17:19:57 +0300

    displayed successful account creation  flash message to login section

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,17 +22,13 @@ def create_user_account():
     form = UserSignUpForm()
 
     if form.validate_on_submit():
-        print 'start adding user'
-
         user = User(email=form.email.data,
                     username=form.username.data,
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
 
-        print 'done adding user'
-
-        flash('Your account has been created successfully.')
+        flash('Your account has been created successfully. You can now Login', 'success')
         return redirect(request.args.get('next') or url_for('auth.login'))
 
     return render_template('user_account.html', form=form)
