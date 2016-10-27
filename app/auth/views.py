9ba2cdb89fa837db7314d766cc3854827c55d2d0
eduhg <edwinnyangena@gmail.com>
2016-10-27 15:35:02 +0300

    added developer signup form to template

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 from . import auth
+from .forms import UserSignUpForm, DeveloperSignUpForm
 
 
 @auth.route('/login')
@@ -10,3 +11,17 @@ def login():
 @auth.route('/create_account')
 def create_account():
     return render_template('create_account.html')
+
+
+@auth.route('/create_account/user')
+def create_user_account():
+    usersignupform = UserSignUpForm()
+
+    return render_template('user_account.html', form=usersignupform)
+
+
+@auth.route('/create_account/developer')
+def create_developer_account():
+    developersignupform = DeveloperSignUpForm(request.form)
+
+    return render_template('developer_account.html', form=developersignupform)
