5f5f28299b51a351a4fdb2859a5fe35aa86f8ab9
eduhg <edwinnyangena@gmail.com>
2016-11-09 21:36:58 +0300

    refactored test function to include coverage based on environment variable 'FLASK_COVERAGE'

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 import os
-import coverage
-import unittest
+cov = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    cov = coverage.coverage(branch=True, include='app/*')
+    cov.start()
+
 from app import create_app, db
-from app.models import User, Developer, Category
-from app.models import Application, ApplicationUpdates
-from app.models import ApplicationAssets
+from app.models import (User, Developer, Category, Application, ApplicationAssets,
+                        ApplicationUpdates)
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -15,43 +18,34 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(
-        app=app, db=db, User=User, Category=Category,
-        Application=Application, Developer=Developer,
-        ApplicationUpdates=ApplicationUpdates,
-        ApplicationAssets=ApplicationAssets)
-
+    return dict(app=app, db=db, User=User, Category=Category, Developer=Developer,
+                Application=Application, ApplicationUpdates=ApplicationUpdates,
+                ApplicationAssets=ApplicationAssets)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
 
 @manager.command
-def test():
+def test(coverage=False):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE'] = '1'
+        os.execvp(sys.executable, [sys.executable] + sys.argv)
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-
-@manager.command
-def test_coverage():
-    """Runs the unit tests with coverage."""
-    cov = coverage.coverage(
-        branch=True,
-        include='app/*'
-    )
-    cov.start()
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    cov.stop()
-    cov.save()
-    print 'Coverage Summary:'
-    cov.report()
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    covdir = os.path.join(basedir, 'coverage')
-    cov.html_report(directory=covdir)
-    cov.erase()
+    if cov:
+        cov.stop()
+        cov.save()
+        print('Coverage Summary:')
+        cov.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'coverage')
+        cov.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        cov.erase()
 
 
 if __name__ == '__main__':
